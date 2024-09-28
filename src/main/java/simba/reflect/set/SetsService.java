41fe4296ee330f935ccc7cf2b27a0d48accbf342
Valentyn Bondarenko <murica.it@gmail.com>
2024-09-28 19:34:03 +0200

    Add test data + 2nd api endpoint

--- src/main/java/simba/reflect/set/SetsService.java
+++ src/main/java/simba/reflect/set/SetsService.java
@@ -2,15 +2,32 @@ package simba.reflect.set;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import simba.reflect.set.dto.SetDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class SetsService {
     private SetsRepo setsRepo;
 
-    public String getAllSets() {
-        return setsRepo.findAll().toString();
+    private final LinkUsersSetsRepository linkUsersSetsRepository;
+
+    public List<SetDto> getAllSetsByUserId(Integer userId) {
+        return linkUsersSetsRepository.findAllSetsByUserId(userId)
+                .stream()
+                .map(set -> new SetDto(
+                        set.getSetID(),
+                        set.getName(),
+                        set.getDescription(),
+                        set.getNextNotification(),
+                        set.getNotificationRepeatTimeHours()
+                ))
+                .collect(Collectors.toList());
     }
+
+
 
     public String getUserSets() {
         return "yee";
